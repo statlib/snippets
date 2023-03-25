46f5716237b3cd88127b1f2c1714155ad1f5ee92
ergodiclife <58880657+ergodiclife@users.noreply.github.com>
2023-03-25 23:47:27 +0000

    Update excel_table.py

--- src/excel_table.py
+++ src/excel_table.py
@@ -1,15 +1,23 @@
 import pandas as pd
 
-
 def write_table(
     df: pd.DataFrame,
     fpath: str,
     sheet_name="Data",
-    col_width=15,
+    max_width=60,
+    col_margin=3,
     h_align: str = "left",
     v_align: str = "top",
     reset_index: bool = True,
 ) -> None:
+    """
+    TODO: Text wrap
+    
+    > df = pd.DataFrame(
+        {"A": "a" * 10, "B": "b" * 40, "C": "c" * 60, "D": "c" * 110}, index=[0]
+    )
+    > write_table(df, "test.xlsx")
+    """
     assert fpath.endswith(".xlsx"), "'fpath' must be an .xlsx file"
     assert isinstance(df, pd.DataFrame), "'df' must be a Pandas DataFrame"
     if reset_index:
@@ -22,10 +30,16 @@ def write_table(
     )
     wb = w.book
     ws = w.sheets[sheet_name]
+    # cell_format = wb.add_format({"text_wrap": True})
+
     d = df.shape
     nr = d[0]
     nc = d[1] - 1
-    col_conf = [{"header": col} for col in df.columns]
+    col_conf = []
+    for i, col in enumerate(df.columns):
+        col_conf.append({"header": col})
+        col_width = min(max_width, df[col].astype(str).map(len).max()) + col_margin
+        ws.set_column(i, i, col_width)
     ws.add_table(0, 0, nr, nc, {"columns": col_conf})
-    ws.set_column(0, nc, col_width)
+    # ws.set_column(0, nc, col_width)
     w.close()
